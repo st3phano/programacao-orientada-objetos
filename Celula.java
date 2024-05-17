c595acf646c02c7102ef77d62f164a0862f9f362
st3phano <31676683+st3phano@users.noreply.github.com>
2024-05-16 22:34:15 -0300

    inicie reformulacao arquitetura

--- Celula.java
+++ Celula.java
@@ -1,50 +1,66 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
-public abstract class Celula {
+public class Celula {
    public static final int ARESTA = 20;
 
-   private int _x;
-   private int _y;
-   private Color _cor;
+   private int _x, _y;
+   private TipoCelula _tipo;
 
-   public Celula(int x, int y, Color cor) {
+   public Celula(int x, int y) {
       set_x(x);
       set_y(y);
-      _cor = cor;
    }
 
-   protected int get_x() {
+   public int get_x() {
       return _x;
    }
 
-   protected void set_x(int x) {
+   private void set_x(int x) {
       _x = x * ARESTA;
    }
 
-   protected void somar_em_x(int valor) {
-      _x += valor * ARESTA;
-   }
-
-   protected int get_y() {
+   public int get_y() {
       return _y;
    }
 
-   protected void set_y(int y) {
+   private void set_y(int y) {
       _y = y * ARESTA;
    }
 
-   protected void somar_em_y(int valor) {
-      _y += valor * ARESTA;
+   public TipoCelula get_tipo() {
+      return _tipo;
    }
 
-   protected Color get_cor() {
-      return _cor;
+   public void set_tipo(TipoCelula tipo) {
+      _tipo = tipo;
    }
 
-   protected void set_cor(Color cor) {
-      _cor = cor;
+   public void desenhar(Graphics g) {
+      switch (get_tipo()) {
+         case VAZIA:
+            g.setColor(Color.WHITE);
+            g.fillRect(get_x(), get_y(), ARESTA, ARESTA);
+            break;
+         case COMIDA:
+            g.setColor(Color.RED);
+            g.fillOval(get_x(), get_y(), ARESTA, ARESTA);
+            break;
+         case CABECA_SNAKE:
+            g.setColor(Color.LIGHT_GRAY);
+            g.fill3DRect(get_x(), get_y(), ARESTA, ARESTA, true);
+            break;
+         case CORPO_SNAKE:
+            g.setColor(Color.DARK_GRAY);
+            g.fill3DRect(get_x(), get_y(), ARESTA, ARESTA, true);
+            break;
+      }
    }
+}
 
-   public abstract void desenhar(Graphics g);
+enum TipoCelula {
+   VAZIA,
+   COMIDA,
+   CABECA_SNAKE,
+   CORPO_SNAKE
 }
